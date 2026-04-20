0c4c89105f492c9b378434622717660d004e95fb
Denzey Mae Acosta <acosta.denzeymae032@gmail.com>
2026-04-21 00:45:32 +0800

    Cors

--- citc_app_backend/settings.py
+++ citc_app_backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'citc_app_backend.urls'
 
 TEMPLATES = [
